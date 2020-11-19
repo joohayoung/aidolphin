872bef65dfe5a07cf44d8b136e643d0ff54e66c5
김신영 <asdfg0816@naver.com>
2020-11-19 17:04:32 +0900

    waveform image

---
+++ waveform_image/music_image.js
@@ -0,0 +1,22 @@
+var wavesurfer = WaveSurfer.create({
+    // 필수 옵션
+    container: '#waveform',
+    // 선택 옵션들 
+    waveColor: 'violet',
+    progressColor: 'purple'
+});
+// <!-- wave객체에 있는 load()함수를 호출-->
+
+// wavesurfer.load(music.fname);
+// window.onload = function() {
+//     var wavesurfer = WaveSurfer.create({
+//         // 필수 옵션
+//         container: '#waveform',
+//         // 선택 옵션들 
+//         waveColor: 'violet',
+//         progressColor: 'purple'
+//         });
+//         // <!-- wave객체에 있는 load()함수를 호출-->
+
+//     // wavesurfer.load(music.fname);
+// }
